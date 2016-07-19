c9eae07ab51953cd5e6b7ac03b18912457e817b6
rawashi <rawashi@syou.cs.tsukuba.ac.jp>
2016-07-19 14:38:25 +0900

    delete listen,mention

--- plugins/listen.py
+++
@@ -1,9 +0,0 @@
-from slackbot.bot import listen_to
-
-@listen_to("tukareta")
-def anzai(message):
-    message.send("otu")
-
-@listen_to("harahe")
-def reaction(message):
-    message.react("+1")
--- plugins/mention.py
+++
@@ -1,6 +0,0 @@
-from slackbot.bot import respond_to
-
-@respond_to("hogehoge")
-@respond_to("nurupo")
-def cheer(message):
-    message.reply("ga!")
